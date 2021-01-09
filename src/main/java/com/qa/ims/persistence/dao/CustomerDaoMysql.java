39effe50ec8d31be35b547a77dbbe63a2318f17c
scostello-hubQA <scostello@academytrainee.com>
2021-01-09 20:55:28 +0000

    creation of dev

--- src/main/java/com/qa/ims/persistence/dao/CustomerDaoMysql.java
+++ src/main/java/com/qa/ims/persistence/dao/CustomerDaoMysql.java
@@ -64,7 +64,7 @@ public class CustomerDaoMysql implements Dao<Customer> {
 	public Customer readLatest() {
 		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
 				Statement statement = connection.createStatement();
-				ResultSet resultSet = statement.executeQuery("SELECT FROM customers ORDER BY id DESC LIMIT 1");) {
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM customers ORDER BY id DESC LIMIT 1");) {
 			resultSet.next();
 			return customerFromResultSet(resultSet);
 		} catch (Exception e) {
